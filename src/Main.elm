06bb538f681e76ecb3a417b23a7bf91d581a0abc
Celso Bonutti Filho <celso.bonuttif@gmail.com>
2020-11-11 19:54:01 -0300

    feat: digimon list

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,17 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, h1, img, text)
-import Html.Attributes exposing (src)
+import Html exposing (Html, div, h1, img, p, text)
+import Html.Attributes exposing (alt, src)
+import Html.Keyed as Keyed
+import Html.Lazy exposing (lazy)
+import Http
+import Json.Decode exposing (Decoder, field, list, string)
 
 
 
 ---- MODEL ----
+
 
 type alias Digimon =
     { name : String
@@ -14,16 +19,21 @@ type alias Digimon =
     , level : String
     }
 
+
+type State
+    = Loading
+    | Data (List Digimon)
+    | Error
+
+
 type alias Model =
-    { digimons : List Digimon
-    }
+    State
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { digimons = []
-      }
-    , Cmd.none
+    ( Loading
+    , loadDigimon
     )
 
 
@@ -32,12 +42,21 @@ init =
 
 
 type Msg
-    = NoOp
+    = GotDigimon (Result Http.Error (List Digimon))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        GotDigimon result ->
+            case result of
+                Ok digimonList ->
+                    ( Data digimonList
+                    , Cmd.none
+                    )
+
+                Err _ ->
+                    ( Error, Cmd.none )
 
 
 
@@ -46,9 +65,47 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
+    case model of
+        Loading ->
+            viewLoading
+
+        Error ->
+            viewError
+
+        Data digimon ->
+            viewData digimon
+
+
+viewLoading : Html msg
+viewLoading =
     div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        [ h1 [] [ text "Loading your Digimons!" ]
+        ]
+
+
+viewError : Html msg
+viewError =
+    div []
+        [ h1 [] [ text "Oops, there was an error loading your Digimons :(" ]
+        ]
+
+
+viewData : List Digimon -> Html msg
+viewData digimonList =
+    Keyed.node "div" [] (List.map viewKeyedDigimon digimonList)
+
+
+viewKeyedDigimon : Digimon -> ( String, Html msg )
+viewKeyedDigimon digimon =
+    ( digimon.name, lazy viewDigimon digimon )
+
+
+viewDigimon : Digimon -> Html msg
+viewDigimon digimon =
+    div []
+        [ img [ src digimon.img, alt digimon.name ] []
+        , h1 [] [ text digimon.name ]
+        , p [] [ text digimon.level ]
         ]
 
 
@@ -64,3 +121,24 @@ main =
         , update = update
         , subscriptions = always Sub.none
         }
+
+
+loadDigimon : Cmd Msg
+loadDigimon =
+    Http.get
+        { url = "https://digimon-api.vercel.app/api/digimon"
+        , expect = Http.expectJson GotDigimon digimonListDecoder
+        }
+
+
+digimonDecoder : Decoder Digimon
+digimonDecoder =
+    Json.Decode.map3 Digimon
+        (field "name" string)
+        (field "img" string)
+        (field "level" string)
+
+
+digimonListDecoder : Decoder (List Digimon)
+digimonListDecoder =
+    list digimonDecoder
